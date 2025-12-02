34e13c0a7ed8ae2e59ff819856841b7d7ece731c
Loris <moine.loris420@gmail.com>
2025-12-02 02:20:17 +0100

    not tested: open graph img add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,18 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="{{ url()->current() }}" />
+        <meta property="og:title" content="@yield('og_title', 'VEM')" />
+        <meta property="og:description" content="@yield('og_description', 'VEM, l\'avenir dans la roche.')" />
+
+        <meta property="og:image" content="@yield('og_image', asset('images/og-default.jpg'))" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="@yield('og_title', 'VEM')" />
+        <meta name="twitter:description" content="@yield('og_description', 'VEM, l\'avenir dans la roche.')" />
+        <meta name="twitter:image" content="@yield('og_image', asset('images/og-default.jpg'))" />
+
         <title>{{ $title ?? config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
