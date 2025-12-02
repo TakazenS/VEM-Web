2609f6c3aa4b06a850a3a14a712ce3da2e216a41
Loris <moine.loris420@gmail.com>
2025-12-02 18:11:45 +0100

    fix: header working well

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,25 +6,33 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:flex">
-                    <x-nav-link :href="route('accueil')">
+                    <x-nav-link :href="route('accueil')" :active="request()->routeIs('accueil')">
                         {{ __('Accueil') }}
                     </x-nav-link>
+                    @auth
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    @endauth
+                    <x-nav-link :href="route('accueil')">
+                        {{ __('Actualité') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('accueil')">
+                        {{ __('Rapports') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('accueil')">
+                        {{ __('Contact') }}
                     </x-nav-link>
                 </div>
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
+                @auth
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            @if (!auth()->user())
-                                <p></p>
-                            @else
-                                <div>{{ Auth::user()->name }}</div>
-                            @endif
+                            <div>{{ Auth::user()->name }}</div>
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
@@ -50,7 +58,18 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+                @endauth
             </div>
+            @if (!auth()->user())
+            <div class="hidden space-x-8 sm:-my-px sm:flex">
+                <x-nav-link :href="route('register')">
+                    {{ __('Inscription') }}
+                </x-nav-link>
+                <x-nav-link :href="route('login')">
+                    {{ __('Connexion') }}
+                </x-nav-link>
+            </div>
+            @endif
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -67,20 +86,31 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('accueil')" :active="request()->routeIs('accueil')">
+                {{ __('Accueil') }}
+            </x-responsive-nav-link>
+            @auth
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            @endauth
+            <x-responsive-nav-link :href="route('accueil')">
+                {{ __('Actualité') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('accueil')">
+                {{ __('Rapports') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('accueil')">
+                {{ __('Contact') }}
             </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
+        @auth
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
-                @if(!auth()->user())
-                    <p></p>
-                @else
-                    <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                    <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-                @endif
+                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
@@ -95,10 +125,11 @@
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        {{ __('Log Out') }}
+                        {{ __('Déconnexion') }}
                     </x-responsive-nav-link>
                 </form>
             </div>
         </div>
+        @endauth
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,11 @@
 <x-app-layout :title="__('VEM | Accueil')">
-        @include('my-components.front.header')
-        <h1>Accueil</h1>
-        @include('my-components.front.footer')
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Accueil') }}
+        </h2>
+    </x-slot>
+
+    <h1>Accueil</h1>
+
+    @include('my-components.front.footer')
 </x-app-layout>
