17f6b7fe40560c9602e1bf1c1aa68773f74e3c7a
Moine Loris <moine.loris420@gmail.com>
2025-12-06 11:23:23 +0100

    Change

--- resources/views/gestion-user.blade.php
+++ resources/views/gestion-user.blade.php
@@ -41,8 +41,8 @@
                 @if (session('success'))
                     <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert" x-data="{ show: true }" x-show="show">
                         <span class="block sm:inline">{{ session('success') }}</span>
-                        <span class="absolute top-0 bottom-0 right-0 px-4 py-3" @click="show = false">
-                            <svg class="fill-current h-6 w-6 text-gray-900" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                        <span class="absolute top-0 bottom-0 right-0 mx-4 my-3" @click="show = false">
+                            <img src="{{ asset('images/assets/cross.svg') }}" alt="Fermer" class="h-8 w-8 cursor-pointer">
                         </span>
                     </div>
                 @endif
@@ -50,7 +50,7 @@
                     <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert" x-data="{ show: true }" x-show="show">
                         <span class="block sm:inline">{{ session('error') }}</span>
                         <span class="absolute top-0 bottom-0 right-0 px-4 py-3" @click="show = false">
-                            <svg class="fill-current h-6 w-6 text-gray-900" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                            <img src="{{ asset('images/assets/cross.svg') }}" alt="Fermer" class="h-8 w-8 cursor-pointer">
                         </span>
                     </div>
                 @endif
@@ -58,7 +58,7 @@
                     <div class="bg-blue-100 border border-blue-400 text-blue-700 px-4 py-3 rounded relative" role="alert" x-data="{ show: true }" x-show="show">
                         <span class="block sm:inline">{{ session('info') }}</span>
                         <span class="absolute top-0 bottom-0 right-0 px-4 py-3" @click="show = false">
-                            <svg class="fill-current h-6 w-6 text-gray-900" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+                            <img src="{{ asset('images/assets/cross.svg') }}" alt="Fermer" class="h-8 w-8 cursor-pointer">
                         </span>
                     </div>
                 @endif
